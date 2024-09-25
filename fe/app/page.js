c114de0716c84b268421f7bab494c6fca438a8d9
A01736897 <a01736897@tec.mx>
2024-09-25 01:44:32 -0600

    Pregunta 2

--- fe/app/page.js
+++ fe/app/page.js
@@ -5,14 +5,34 @@ import React, { useEffect, useState } from 'react';
 
 
 export default function Home() {
-  const [posX, setPosX] = useState(6);
-  const [posY, setPosY] = useState(8);
+  const [posX, setPosX] = useState(0);
+  const [posY, setPosY] = useState(0);
+
+  let matrix = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0],
+  [0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+  [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0],
+  [0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 
   useEffect(() => {
     const interval = setInterval(() => {
       fetch("http://localhost:8000/run")
+      
       .then(res => res.json())
       .then(res => {
+        console.log("Nueva posición del fantasma:", res.agents[0].pos);
+        console.log("Datos recibidos:", res);
         setPosX(res.agents[0].pos[0]-1);
         setPosY(res.agents[0].pos[1]-1);
       });
@@ -22,10 +42,15 @@ export default function Home() {
   }, [posX, posY]);
 
   return (
-    <div className={styles.page}>
-      PacMan
-      <svg width="800" height="500" style={{backgroundColor: "lightgray"}} xmlns="http://www.w3.org/2000/svg">
-        <image x={255 + 25 * posX} y={9 + 25 * posY} href="ghost.png"/>
+    <div>
+      <svg width="800" height="500" xmlns="http://www.w3.org/2000/svg">
+      {
+        matrix.map((row, rowidx) =>
+          row.map((value, colidx) =>
+            <rect x={250 + 25 * colidx} y={5 + 25 * rowidx} width={25} height={25} fill={value == 1 ? "lightgray" : "gray"}/>
+      ))
+      }
+      <image x={255 + 25 * posX} y={9 + 25 * posY} href="ghost.png"/>
       </svg>
     </div>
   );
--- pacman.jl
+++ pacman.jl
@@ -1,19 +1,61 @@
 using Agents
 using CairoMakie
 
+matrix = [
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+    0 1 1 1 1 1 1 1 0 1 1 1 1 1 1 1 0;
+    0 1 0 1 0 0 0 1 1 1 0 1 0 1 0 1 0;
+    0 1 1 1 0 1 0 0 0 0 0 1 0 1 1 1 0;
+    0 1 0 0 0 1 1 1 1 1 1 1 0 0 0 1 0;
+    0 1 0 1 0 1 0 0 0 0 0 1 1 1 0 1 0;
+    0 1 1 1 0 1 0 1 1 1 0 1 0 1 0 1 0;
+    0 1 0 1 0 1 0 1 1 1 0 1 0 1 0 1 0;
+    0 1 0 1 1 1 0 0 1 0 0 1 0 1 1 1 0;
+    0 1 0 0 0 1 1 1 1 1 1 1 0 0 0 1 0;
+    0 1 1 1 0 1 0 0 0 0 0 1 0 1 1 1 0;
+    0 1 0 1 0 1 0 1 1 1 0 0 0 1 0 1 0;
+    0 1 1 1 1 1 1 1 0 1 1 1 1 1 1 1 0;
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+]
+
+
+movimientos = [
+    (0, 1),  
+    (1, 0),  
+    (0, -1), 
+    (-1, 0)  
+]
+movimientos_v = []
+
 @agent struct Ghost(GridAgent{2})
     type::String = "Ghost"
 end
 
 function agent_step!(agent, model)
-    randomwalk!(agent, model)
+    movimientos_v = []
+
+    for move in movimientos
+        (dx, dy) = move
+        (x, y) = agent.pos .+ (dx, dy)
+
+        if inbounds((x, y), model) && matrix[y, x] == 1
+            push!(movimientos_v, (dx, dy)) 
+        end
+    end
+
+    if !isempty(movimientos_v)
+        move = rand(movimientos_v)
+        (dx, dy) = move
+        new_pos = (agent.pos[1] + dx, agent.pos[2] + dy) 
+        move_agent!(agent, new_pos, model)
+    end
 end
 
 function initialize_model()
-    space = GridSpace((5,5); periodic = false, metric = :manhattan)
+    space = GridSpace(size(matrix); periodic = false, metric = :manhattan)
     model = StandardABM(Ghost, space; agent_step!)
     return model
 end
 
 model = initialize_model()
-a = add_agent!(Ghost, pos=(3, 3), model)
+a = add_agent!(Ghost, pos=(1, 1), model)
